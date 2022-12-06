8cff0e20a6592df1e2c25c6383780f9e2ef235d4
Giorgi Gobechia <82951209+giorgigobechia@users.noreply.github.com>
2022-12-06 15:54:32 +0400

    create body section

--- components/User.tsx
+++ components/User.tsx
@@ -5,13 +5,13 @@ export default function User() {
     if(session){
         return (
             <>
-             <img onClick={signOut} src={session?.user?.image} alt="" style={{height: "40px",width: "40px",borderRadius: "180%"}} />
+             <img onClick={signOut} src={session?.user?.image} alt="" className="h-10 w-10 rounded-full hover:bg-gray-200 cursos-pointer p-1" />
             </>
         )
     }
     return(
         <>
-        <button onClick={signIn}>Sign In</button>
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md" onClick={signIn}>Sign In</button>
         </>
     )
 }
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -4,12 +4,12 @@ export default function signin({providers}){
     return (
         <>
             <Header/>
-            <div className="">
+            <div className="mt-40">
                 {Object.values(providers).map(provider =>(
-                    <div key={provider?.name} className="">
-                          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png" alt="google-logo" />
-                          <p>This website is created for learning purposes</p>
-                          <button onClick={()=>signIn(provider?.id,{callbackUrl: "/"})}>Sign in with {provider?.name} </button>
+                    <div key={provider?.name} className="flex flex-col items-center">
+                          <img className="w-52 object-cover" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png" alt="google-logo" />
+                          <p className="text-sm italic my-10 text-center">This website is created for learning purposes</p>
+                          <button className="bg-red-500 rounded-lg text-white p-3 hover:bg-red-600" onClick={()=>signIn(provider?.id,{callbackUrl: "/"})}>Sign in with {provider?.name} </button>
                     </div>
                 ))}
             </div>
